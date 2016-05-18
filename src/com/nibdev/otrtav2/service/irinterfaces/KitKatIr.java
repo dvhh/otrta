641a218a73e87b7f339c7d67664e7bd88c88bf26
nibdev <niklaus.bruno@gmail.com>
2016-05-18 16:42:44 +0200

    Fix for Android Consumer IR Api >= SDK21
    (thx @ Vardion
     http://forum.xda-developers.com/showpost.php?p=65144177&postcount=55)

--- src/com/nibdev/otrtav2/service/irinterfaces/KitKatIr.java
+++ src/com/nibdev/otrtav2/service/irinterfaces/KitKatIr.java
@@ -3,6 +3,7 @@ package com.nibdev.otrtav2.service.irinterfaces;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.hardware.ConsumerIrManager;
+import android.os.Build;
 
 import com.nibdev.otrtav2.service.IrDataCompat;
 
@@ -10,7 +11,7 @@ import com.nibdev.otrtav2.service.IrDataCompat;
 public class KitKatIr extends IrFace {
 
 	private ConsumerIrManager mKkIr;
-	
+
 	public KitKatIr(Context c) throws Exception{
 		mKkIr = (ConsumerIrManager) c.getSystemService(Context.CONSUMER_IR_SERVICE);
 		if (!mKkIr.hasIrEmitter()) throw new Exception("No KitKat IR Device");
@@ -29,7 +30,14 @@ public class KitKatIr extends IrFace {
 
 	@Override
 	protected void ifSendCode(IrDataCompat data, boolean repeat) {
-		mKkIr.transmit(data.getFrequency(), data.getFrame());
+		final int freq = data.getFrequency();
+		final int[] frame = data.getFrame();
+		if (Build.VERSION.SDK_INT >= 21) {
+			for (int i = 0; i < frame.length; i++){
+				frame[i] = frame[i] * 1000000 / freq;
+			}			
+		}
+		mKkIr.transmit(freq, frame);
 
 	}
 
